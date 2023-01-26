bc947a6c313cd761c22fd2cb5f74510dfaa1d8b6
ellioth <elliot.houdjal@gmail.com>
2023-01-26 19:00:30 +0100

    Register Page Created (not functionnal)

---
+++ FrontEnd_WebApp/src/routes/login/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+    import { enhance } from '$app/forms';
+    /** @type {import('./$types').ActionData} */
+    export let form;
+</script>
+
+<svelte:head>
+    <title>Login</title>
+    <meta charset="UTF-8">
+
+</svelte:head>
+
+<body>
+
+<h1>Login</h1>
+<div>
+    <form use:enhance method="POST">
+        <fieldset >
+            <input
+
+                    name="username"
+                    type="text"
+                    required
+                    placeholder="Your Name"
+            />
+        </fieldset>
+
+        <fieldset >
+            <input
+
+                    name="password"
+                    type="password"
+                    required
+                    placeholder="Password"
+            />
+        </fieldset>
+        <button type="submit"q>Sign up</button>
+    </form>
+<a>Already have an account ?</a>
+</div>
+</body>
---
+++ FrontEnd_WebApp/src/routes/register/+page.svelte
@@ -0,0 +1,33 @@
+
+
+<script>
+    import { enhance } from '$app/forms';
+    /** @type {import('./$types').ActionData} */
+    export let form;
+</script>
+
+<svelte:head>
+    <title>Sign in</title>
+</svelte:head>
+
+<form use:enhance method="POST">
+    <fieldset>
+        <input
+
+                name="username"
+                type="text"
+                required
+                placeholder="Your Name"
+        />
+    </fieldset>
+    <fieldset >
+        <input
+
+                name="password"
+                type="password"
+                required
+                placeholder="Password"
+        />
+    </fieldset>
+    <button type="submit">Sign in</button>
+</form>
