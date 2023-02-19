1a001456e46ba8936bcaf4e1f672825a01c8f196
ellioth <elliot.houdjal@gmail.com>
2023-02-19 13:49:41 +0100

    Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -9,12 +9,12 @@ const config = {
 
 export default config;
 
+
 import adapter from '@sveltejs/adapter-vercel';
 
 export default {
 	kit: {
-		adapter: adapter({
-			// see the 'Deployment configuration' section below
-		})
+		adapter: adapter(),
+		// other configuration options
 	}
 };
